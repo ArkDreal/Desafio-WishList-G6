688d56e28bae2e6ef349d6f91aa211d21e2e3fff
Gustavo Barros <gustavo.viaro@portalsesisp.org.br>
2021-10-26 16:04:47 -0300

    bd finalizado

---
+++ BD/DML_WishList.sql
@@ -0,0 +1,10 @@
+USE WishList
+GO
+
+INSERT INTO Usuario (email, senha)
+VALUES ('gubarros@email.com', 'gubarros123'),('arkdreal@email.com', 'arkdreal17'), ('raulfiorin@email.com','raul123'),('jhozapaza@email.com', 'jhon123');
+GO
+
+INSERT INTO Desejos(descricao)
+VALUES ('Playstation 5'), ('conhecer a Noruega'), ('comprar uma fiorino'), ('jogar futebol com o Neymar');
+go
